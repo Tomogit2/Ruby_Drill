c05442fe85076b470715aa977b6d996205a46bf0
Tomogit2 <tomotomotomotomo_@hotmail.co.jp>
2024-05-28 08:59:46 +0900

    16_2

--- 16.rb
+++ 16.rb
@@ -30,3 +30,29 @@ class Fruit
 
 
  3つのインスタンスを生成し、クラスメソッドを呼び出し、「採れたて新鮮な果実です」と表示し、インスタンス毎にインスタンスメソッドを呼び出し、「【名前】は【価格】円です」と表示したいのですが…もう何があってるか間違ってるのか、そもそもの方向性が違いそうで困っています。
+
+ #模範解答
+ class Fruit
+
+  def self.fresh
+    puts "採れたて新鮮な果実です"
+  end
+ 
+  def initialize(name, price)
+    @name = name
+    @price = price
+  end
+ 
+  def introduce
+    puts "#{@name}は#{@price}円です"
+  end
+ end
+ 
+ apple = Fruit.new("リンゴ", 120)
+ orange = Fruit.new("オレンジ", 200)
+ strawberry = Fruit.new("イチゴ", 60)
+ 
+ Fruit.fresh
+ apple.introduce
+ orange.introduce
+ strawberry.introduce
