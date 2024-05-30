a2e64e0424845ba86308e77db8fa8591ad63cc61
Tomogit2 <tomotomotomotomo_@hotmail.co.jp>
2024-05-31 07:53:49 +0900

    18

--- 17.rb
+++ 17.rb
@@ -7,9 +7,20 @@ def missing_char(str, n)
 end
 
 # 呼び出し例（1つ目の引数には対象となる文字列を、2つ目の引数には対象となる文字列から何番目の文字を削除するのかを指定しましょう）
-missing_char('kitten', 1)
+def missing_char(str, n)
 
 string = "abcdefg"
 string.slice!(2)
 
 puts string
+end
+
+#模範解答
+
+def missing_char(str, n)
+  str.slice!(n - 1)
+  puts str
+end
+
+# 呼び出し例
+missing_char('kitten', 1)
