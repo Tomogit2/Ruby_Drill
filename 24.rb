7f4a871e22f3823bdcc0ce0c2b5ad798df24afa9
Tomogit2 <tomotomotomotomo_@hotmail.co.jp>
2024-06-21 08:43:48 +0900

    24_2

--- 24.rb
+++ 24.rb
@@ -16,3 +16,15 @@ def sleep_in(is_weekday, is_vacation)
     false
   end
 end
+
+模範解答
+def sleep_in(is_weekday, is_vacation)
+  if (is_weekday != true) || (is_vacation == true)
+    puts true
+  else
+    puts false
+  end
+end
+
+# 呼び出し例
+sleep_in(false, false)
